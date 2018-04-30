777dccba7d58b8dd7b2a96ed23ef29b88d8080f9
Adrian Miguel <adrian.miguel@tajamar365.com>
2018-04-30 19:01:17 +0200

    comit

---
+++ source/repos/ClienteColoresJson/ClienteColoresJson/Controllers/ColoresController.cs
@@ -0,0 +1,58 @@
+﻿using ClienteColoresJson.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClienteColoresJson.Controllers
+{
+    public class ColoresController : Controller
+    {
+        ModeloColores modelo;
+
+        public ColoresController()
+        {
+            modelo = new ModeloColores();
+        }
+
+        // GET: ClientesController 
+        public async Task<ActionResult> Index()
+        {
+            List<Color> lista = await modelo.MostrarColores();
+            return View(lista);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Color color)
+        {
+            await modelo.InsertarColor(color.IdColor, color.Nombre, color.Hex);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Edit(int idcolor)
+        {
+            Color color = await modelo.BuscarColor(idcolor);
+            return View(color);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(Color color)
+        {
+            await modelo.ModificarColor(color.IdColor, color.Nombre,color.Hex);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Delete(int idcolor)
+        {
+            await modelo.EliminarColor(idcolor);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ source/repos/ClienteColoresJson/ClienteColoresJson/Models/ModeloColores.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ClienteColoresJson.Models
+{
+    public class ModeloColores
+    {
+        public async Task<List<Color>> MostrarColores()
+        {
+            using (HttpClient cliente = new HttpClient())
+            {
+                String peticion = "api/Colores/";
+                cliente.BaseAddress = new Uri("http://localhost:50014/");
+                cliente.DefaultRequestHeaders.Accept.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await cliente.GetAsync(peticion);
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Color> colores = await response.Content.ReadAsAsync<List<Color>>();
+                    return colores;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+
+        public async Task<Color> BuscarColor(int idcolor)
+        {
+            using (HttpClient cliente = new HttpClient())
+            {
+                String peticion = "api/Colores/" + idcolor;
+                cliente.BaseAddress = new Uri("http://localhost:50014/");
+                cliente.DefaultRequestHeaders.Accept.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await cliente.GetAsync(peticion);
+                if (response.IsSuccessStatusCode)
+                {
+                    Color color = await response.Content.ReadAsAsync<Color>();
+                    return color;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+
+        public async Task InsertarColor(int idcolor, String nombre, String hexa)
+        {
+            using (HttpClient cliente = new HttpClient())
+            {
+                String peticion = "api/NuevoColor/";
+                cliente.BaseAddress = new Uri("http://localhost:50014/");
+                cliente.DefaultRequestHeaders.Accept.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                Color color = new Color()
+                {
+                    IdColor = idcolor,
+                    Nombre = nombre,
+                    Hex = hexa
+                };
+                HttpResponseMessage response = await cliente.PostAsJsonAsync(peticion, color);
+            }
+        }
+
+
+        public async Task ModificarColor(int idcolor, String nombre, String hex)
+        {
+            using (HttpClient cliente = new HttpClient())
+            {
+                String peticion = "api/ModificarColor/";
+                cliente.BaseAddress = new Uri("http://localhost:50014/");
+                cliente.DefaultRequestHeaders.Accept.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                Color cli = new Color()
+                {
+                    IdColor = idcolor,
+                    Nombre = nombre,
+                    Hex = hex
+                };
+                HttpResponseMessage response =
+                    await cliente.PutAsJsonAsync(peticion, cli);
+            }
+        }
+
+
+        public async Task EliminarColor(int idcolor)
+        {
+            using (HttpClient cliente = new HttpClient())
+            {
+                String peticion = "api/EliminarColor/" + idcolor;
+                cliente.BaseAddress = new Uri("http://localhost:50014/");
+                cliente.DefaultRequestHeaders.Accept.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response =
+                    await cliente.DeleteAsync(peticion);
+            }
+        }
+    }
+}
